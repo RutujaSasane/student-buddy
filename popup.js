a298f6326e8d76606368a9a41a15790a7c0cc812
RutujaSasane <rutujas2004@gmail.com>
2026-07-15 21:03:16 +0530

    Handle Start Learning Button click event

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-console.log("popup.js loaded sucessfully");
+console.log("popup.js loaded successfully");
+
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", function() {
+    console.log("Start Learning Clicked");
+});
